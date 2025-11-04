9aa5ecd3c78b07a504faced7930632002a427b3a
Hippolyte <etyl.sani@gmail.com>
2025-11-04 10:03:54 +0100

    CLN removed dpir2c

--- solvers/dpir.py
+++ solvers/dpir.py
@@ -5,12 +5,6 @@ with safe_import_context() as import_ctx:
     from torch.utils.data import DataLoader
     import deepinv as dinv
     import numpy as np
-    from deepinv.optim import BaseOptim
-    from deepinv.optim.prior import PnP
-    from deepinv.optim.data_fidelity import L2
-    from deepinv.optim.optimizers import create_iterator
-    from deepinv.optim.dpir import get_DPIR_params
-    from benchmark_utils.denoiser_2c import Denoiser_2c
     from tqdm import tqdm
     from benchmark_utils.helper import get_device
 
@@ -38,16 +32,10 @@ class Solver(BaseSolver):
         best_sigma = 0
         best_psnr = 0
 
-        # If the number of channels is 2 we use a custom DPIR solver
-        if self.image_size[0] == 2:
-            model_class = DPIR_2C
-        else:
-            model_class = dinv.optim.DPIR
-
         # If the number of channels is different from 1 or 3
         # then we can't use pretrained DRUNet
         for sigma in np.linspace(0.01, 0.1, 10):
-            model = model_class(sigma=sigma, device=self.device)
+            model = dinv.optim.DPIR(sigma=sigma, device=self.device)
 
             psnr = []
 
@@ -68,24 +56,8 @@ class Solver(BaseSolver):
                 best_sigma = sigma
                 best_psnr = psnr
 
-            self.model = model_class(sigma=best_sigma, device=self.device)
+            self.model = dinv.optim.DPIR(sigma=best_sigma, device=self.device)
         self.model.eval()
 
     def get_result(self):
         return dict(model=self.model, model_name="DPIR", device=self.device)
-
-
-# Custom DPIR solver with 2 channels
-class DPIR_2C(BaseOptim):
-    def __init__(self, sigma=0.1, device="cuda"):
-        prior = PnP(denoiser=Denoiser_2c(device=device))
-        sigma_denoiser, stepsize, max_iter = get_DPIR_params(sigma)
-        params_algo = {"stepsize": stepsize, "g_param": sigma_denoiser}
-        super(DPIR_2C, self).__init__(
-            create_iterator("HQS", prior=prior, F_fn=None, g_first=False),
-            max_iter=max_iter,
-            data_fidelity=L2(),
-            prior=prior,
-            early_stop=False,
-            params_algo=params_algo,
-        )
